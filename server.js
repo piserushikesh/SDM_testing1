abe0f1788a3d591a8c43e3f2f19e4c7b4815f896
rushikesh <rpise4938@gmail.com>
2023-01-02 00:16:38 +0530

    changes

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 var express=require('express');
 var app = express();
 var path =require("path");
-var fs=require("fs");
+//var fs=require("fs");
 
-app.use(express.static(path.join(__dirname,'public')));
+// app.use(express.static(path.join(__dirname,'public')));
 
 
-app.get('/',function (req,res){
+// app.get('/',function (req,res){
 
-    res.sendFile(path.join(__dirname + '/index.html'));
+//     res.sendFile(path.join(__dirname + '/index.html'));
+// });
+
+app.get('/',(req,res)=>{
+    res.sendFile('public/index.html',{root:__dirname})
 });
 
 app.get('/about_us',function(req,res){
@@ -17,8 +21,9 @@ app.get('/about_us',function(req,res){
     res.send(person);
 });
 
-var server=app.listen(5000,function(){
-
-    var host=server.address().address
-    var port=server.address().port
+app.get('/contactus',(req,res)=>{
+    var number={num:8605145620};
+   res.send(number);
 });
+app.listen(5000);
+console.log("startted");
